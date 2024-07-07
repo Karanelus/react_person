1e508a10f0c4ae8681218bee0cafb49efc89ba88
Rusłan Karaniewski <karane88vka@gmail.com>
2024-07-07 12:43:39 +0200

    My solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,26 @@
-// export const Person = ({ person }) => ();
+import React from 'react';
+import { PersonName } from './PersonName';
+import { PersonAge } from './PersonAge';
+import { PersonSpouse } from './PersonSpouse';
+
+export const Person = ({ person }) => {
+  return (
+    <section className="Person">
+      <PersonName name={person.name} />
+      {person.age && <PersonAge age={person.age} />}
+      <PersonSpouse
+        sex={person.sex}
+        spouse={person.partnerName}
+        isMarried={person.isMarried}
+      />
+    </section>
+  );
+};
+
+// misha = {
+//   name: 'Misha',
+//   age: 37,
+//   sex: 'm',
+//   isMarried: true,
+//   partnerName: 'Natasha',
+// };
